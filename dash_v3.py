8a13ac86a33003b0514ccd1554da51d3805e8672
edwardobrusin <edwardobrusin@gmail.com>
2026-08-27 13:57:25 -0600

    27 de agosto

--- dash_v3.py
+++ dash_v3.py
@@ -650,7 +650,7 @@ else:
 # ==========================================================================
 # 3. HEADER PRINCIPAL
 # ==========================================================================
-fecha_revision = "25 de Agosto de 2026"
+fecha_revision = "27 de Agosto de 2026"
 st.markdown(f"""
 <div class="main-header" style="display: flex; justify-content: space-between; align-items: center;">
     <div style="display: flex; flex-direction: column; justify-content: center;">
